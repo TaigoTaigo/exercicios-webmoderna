4d5eaf3dee29dd5a548f1e3c368e6329b4ea98b9
Taigo <dev@tiagogassner.com>
2021-11-13 17:50:07 -0300

    Aula HTML/formulario | Concluida!

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -7,7 +7,13 @@ app.use(express.urlencoded({ extended: true }))
 
 app.post("/usuarios", (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabéns!</h1>')
+    resp.send('<h1>Parabéns. Usuario incluido!</h1>')
+})
+
+app.post("/usuarios/:id", (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuario alterado!</h1>')
 })
 
 app.listen(3003)
